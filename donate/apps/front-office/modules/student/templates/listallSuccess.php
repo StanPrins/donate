b104087f4f7550e68fcd39d56997d330102a50f3
ericsson.symfony@gmail.com <ericsson.symfony@gmail.com@25e0429c-7ac5-11de-96f2-af6933d8d261>
2009-07-29 01:00:40 +0000

    listall is ok

--- donate/apps/front-office/modules/student/templates/listallSuccess.php
+++ donate/apps/front-office/modules/student/templates/listallSuccess.php
@@ -3,9 +3,51 @@
 // date: 2009/07/19 21:34:06
 ?>
 <div id="sf_admin_container">
-<h1>所有学生信息</h1>
-
 <div id="sf_admin_content">
+<h1>所有学生信息</h1>
+<?php
+ use_helper('Javascript');
+ echo form_tag('student/listall','name="Find"');?> 
+<table border=0>
+<tr><td><strong>地区：</strong></td>
+<td>
+<select name="site_id">
+  <option value="-1">--请选择--</option>
+  <?php foreach($projectsites as $projectsite):?>
+  <option value=<?php echo $projectsite[0]?> <?php if(!empty($site_id) && $site_id==$projectsite[0]) echo "selected"; ?>>
+  <?php echo $projectsite[1]?>
+  </option>
+  <?php endforeach;?>
+</select>
+</td>
+<td><strong>学校：</strong></td>
+<td>
+<select name="school_id" <?php if($school_count < 1) echo "disabled"; ?>>
+<option value="-1">--请选择--</option>
+<?php foreach($schools as $school):?>
+<option value=<?php echo $school[0]?> <?php if(!empty($school_id) && ($school_id==$school[0])) echo "selected"; ?>>
+<?php echo $school[1]?>
+</option>
+<?php endforeach;?>
+</select>
+</td>
+<td><strong>姓名：</strong></td>
+<td>
+<?php 
+$default_name = empty($student_name)?'':$student_name;
+$default_school_id = empty($school_id)?-1:$school_id;
+$default_site_id = empty($site_id)?-1:$site_id;
+ echo input_auto_complete_tag('student_name',$default_name,
+		'student/autocomplete?school_id='.$default_school_id.'&site_id='.$default_site_id,
+ 		array('autocomplete'=>'on'),
+ 		array('use_style'=>true));
+ echo submit_tag('查找');
+ ?>
+ </td>
+ </tr>
+ </table>
+ </form>
+<?php if(sizeof($pager->getResults())>0):?>
 <table class="sf_student_list">
 	<tbody>
 	<?php foreach ($pager->getResults() as $student): ?>
@@ -45,7 +87,16 @@
 	</tbody>
 </table>
 
-		<?php include_partial('pager',array('pager' => $pager, 'page_to_link' => 'listall' ))?>
-
+<?php include_partial('listpager',array('pager' => $pager, 'page_to_link' => 'listall', 'school_id'=>$default_school_id, 'site_id'=>$default_site_id ))?>
+<?php else:?>
+<table class="sf_student_list">
+<tr><td align="center">无相关记录</td></tr>
+</table>
+<?php endif;?>
 </div>
+<?php echo observe_form('Find',array(
+ 		'update'=>'sf_admin_content',
+ 		'url'=>'student/listall',
+ 		'with'=>"Form.serialize('Find')",
+ 		'script'=>true))?>
 </div>
