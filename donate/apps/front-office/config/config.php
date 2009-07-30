8df823cc148186ac82db3753fa1c5b1a1ba2730d
ericsson.symfony <ericsson.symfony@gmail.com>
2009-07-30 04:15:57 +0000

    sfImage

--- donate/apps/front-office/config/config.php
+++ donate/apps/front-office/config/config.php
@@ -6,3 +6,5 @@ include(SF_ROOT_DIR.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php
 // symfony bootstraping
 require_once($sf_symfony_lib_dir.'/util/sfCore.class.php');
 sfCore::bootstrap($sf_symfony_lib_dir, $sf_symfony_data_dir);
+sfConfig::set('sf_image_width',40);
+sfConfig::set('sf_image_height',40);
