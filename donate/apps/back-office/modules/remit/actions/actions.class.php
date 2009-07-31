de654e1a3c0a3643fc547eff96fee2e86b4d3867
ericsson.symfony <ericsson.symfony@gmail.com>
2009-07-31 09:12:29 +0000

    Share project "donate" into "https://donate.googlecode.com/svn"

---
+++ donate/apps/back-office/modules/remit/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * remit actions.
+ *
+ * @package    donate
+ * @subpackage remit
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class remitActions extends autoremitActions
+{
+}
