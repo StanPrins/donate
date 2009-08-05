8997befa95ab0beec2f7b8c1cccae7ecbf711b59
ericsson.symfony@gmail.com <ericsson.symfony@gmail.com@25e0429c-7ac5-11de-96f2-af6933d8d261>
2009-08-05 08:56:20 +0000

    Share project "donate" into "https://donate.googlecode.com/svn"

---
+++ donate/apps/back-office/modules/reportcard/actions/actions.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * reportcard actions.
+ *
+ * @package    donate
+ * @subpackage reportcard
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2288 2006-10-02 15:22:13Z fabien $
+ */
+class reportcardActions extends autoreportcardActions
+{
+}
