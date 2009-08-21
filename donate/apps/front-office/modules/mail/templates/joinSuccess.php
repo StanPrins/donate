2609fa3ee38351b78550d6856e9db087162a9e48
guoxirong <guoxirong@gmail.com>
2009-08-21 07:13:41 +0000

    邮件发送

---
+++ donate/apps/front-office/modules/mail/templates/joinSuccess.php
@@ -0,0 +1,15 @@
+<p>亲爱的会员：</p>
+
+<p>您好！</p>
+
+<p>感谢您的加入，奉献自己的爱心！</p> 
+<p>您已通过审核，您的用户名和密码是：</p>
+ 
+<p>
+用户名: <strong><?php echo $username ?></strong><br/>
+密码: <strong><?php echo $password ?></strong>
+</p>
+
+<p>赶紧行动吧， <?php echo link_to('登录', '@login') ?>！</p>
+ 
+<p><img src="cid:CID1" /></p>
---
+++ donate/apps/front-office/modules/mail/templates/registerSuccess.php
@@ -0,0 +1,15 @@
+<p>亲爱的会员：</p>
+
+<p>您好！</p>
+ 
+<p>您已经注册成功，您的用户名和密码是：</p>
+ 
+<p>
+用户名: <strong><?php echo $username ?></strong><br/>
+密码: <strong><?php echo $password ?></strong>
+</p>
+ 
+<p>我们将尽快地完成您的身份复核并邮件通知您,敬请期待!</p>
+<p>您也可以随时<?php echo link_to('登录', '@login') ?>帐户，察看自己的审核状态。</p>
+ 
+<p><img src="cid:CID1" /></p>
