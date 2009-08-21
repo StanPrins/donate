75319cd8ad007e756a908eb790c47ccc30697efb
guoxirong <guoxirong@25e0429c-7ac5-11de-96f2-af6933d8d261>
2009-08-21 12:57:17 +0000

    邮件

--- donate/apps/front-office/modules/mail/templates/registerSuccess.php
+++ donate/apps/front-office/modules/mail/templates/registerSuccess.php
@@ -10,6 +10,6 @@
 </p>
  
 <p>我们将尽快地完成您的身份复核并邮件通知您,敬请期待!</p>
-<p>您也可以随时<?php echo link_to('登录', '@login') ?>帐户，察看自己的审核状态。</p>
+<p>您也可以随时<a href="http://www.ourfreesky.org/kids" target="_blank">登录</a>帐户，察看自己的审核状态。</p>
  
 <p><img src="cid:CID1" /></p>
